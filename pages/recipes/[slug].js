6a5406641798234cf6991802a5454b441c21de8c
KevinSig <41274608+KevinSig@users.noreply.github.com>
2021-07-23 18:12:26 -0400

    saving

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,56 +1,151 @@
+// import { createClient } from 'contentful'
+// import Image from 'next/image'
+// import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+
+// const client = createClient({
+//   space: process.env.CONTENTFUL_SPACE_ID, //porcess is from the vs code itself
+//   accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+// })
+
+// export const getStaticPaths = async () => {
+//   // this is done at build time to get generate the paths
+
+//   const res = await client.getEntries({
+//     content_type: 'recipe',
+//   })
+//   const paths = res.items.map(item => {
+//     return {
+//       params: { slug: item.fields.slug },
+//     }
+//   })
+
+//   //returns an array of objects that are the different paths
+//   //fallback false is a 404 page
+
+//   return {
+//     paths: paths,
+//     fallback: false,
+//   }
+// }
+
+// export async function getStaticProps({ params }) {
+//   // this is to fetch a single page, and will ingject into Recipe Details
+//   // get params from the context
+//   //destructuring to get items from the array
+//   const { items } = await client.getEntries({
+//     content_type: 'recipe',
+//     'fields.slug': params.slug,
+//   })
+//   return {
+//     props: { recipe: items[0], AMIR: 'HI' },
+//     revalidate: 1
+//   }
+
+//   //only after 10 seconds does it reach out to the content storge
+// }
+
+// export default function RecipeDetails({ recipe, AMIR }) {
+//   console.log(AMIR)
+//   console.log(recipe)
+//   // finding out all the paths and routes that will use this component as their page
+
+//   const { featuredImage, title, cookingTime, ingredients, method } =
+//     recipe.fields
+//   return (
+//     <div>
+//       <div className='banner'>
+//         <Image
+//           src={'https:' + featuredImage.fields.file.url}
+//           width={featuredImage.fields.file.details.image.width}
+//           height={featuredImage.fields.file.details.image.height}
+//         />
+//         <h2>{title}</h2>
+//       </div>
+//       <div className='info'>
+//         <p> Takes about {cookingTime} minutes</p>
+//         <h3>Ingredients</h3>
+//         {ingredients.map(ingredient => (
+//           <span key={ingredient}>{ingredient}</span>
+//         ))}
+//       </div>
+
+//       <div className='method'>
+//         <h3>Method:</h3>
+//         <div>{documentToReactComponents(method)}</div>
+//       </div>
+//       <style jsx>{`
+//         h2,
+//         h3 {
+//           text-transform: uppercase;
+//         }
+//         .banner h2 {
+//           margin: 0;
+//           background: #fff;
+//           display: inline-block;
+//           padding: 20px;
+//           position: relative;
+//           top: -60px;
+//           left: -10px;
+//           transform: rotateZ(-1deg);
+//           box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.1);
+//         }
+//         .info p {
+//           margin: 0;
+//         }
+//         .info span::after {
+//           content: ', ';
+//         }
+//         .info span:last-child::after {
+//           content: '.';
+//         }
+//       `}</style>
+//     </div>
+//   )
+// }
+
 import { createClient } from 'contentful'
-import Image from 'next/image'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Image from 'next/image'
 
 const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID, //porcess is from the vs code itself
+  space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_KEY,
 })
 
 export const getStaticPaths = async () => {
-  // this is done at build time to get generate the paths
-
   const res = await client.getEntries({
     content_type: 'recipe',
   })
+
   const paths = res.items.map(item => {
     return {
       params: { slug: item.fields.slug },
     }
   })
 
-  //returns an array of objects that are the different paths
-  //fallback false is a 404 page
-
   return {
-    paths: paths,
+    paths,
     fallback: false,
   }
 }
 
-export async function getStaticProps({ params }) {
-  // this is to fetch a single page, and will ingject into Recipe Details
-  // get params from the context
-  //destructuring to get items from the array
+export const getStaticProps = async ({ params }) => {
   const { items } = await client.getEntries({
     content_type: 'recipe',
     'fields.slug': params.slug,
   })
-  return {
-    props: { recipe: items[0], AMIR: 'HI' },
-    revalidate: 1
-  }
 
-  //only after 10 seconds does it reach out to the content storge 
+  return {
+    props: { recipe: items[0] },
+    revalidate: 1,
+  }
 }
 
-export default function RecipeDetails({ recipe, AMIR }) {
-  console.log(AMIR)
-  console.log(recipe)
-  // finding out all the paths and routes that will use this component as their page
-
+export default function RecipeDetails({ recipe }) {
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields
+  console.log(method)
+
   return (
     <div>
       <div className='banner'>
@@ -61,11 +156,13 @@ export default function RecipeDetails({ recipe, AMIR }) {
         />
         <h2>{title}</h2>
       </div>
+
       <div className='info'>
-        <p> Takes about {cookingTime} minutes</p>
-        <h3>Ingredients</h3>
-        {ingredients.map(ingredient => (
-          <span key={ingredient}>{ingredient}</span>
+        <p>Takes about {cookingTime} mins to cook.</p>
+        <h3>Ingredients:</h3>
+
+        {ingredients.map(ing => (
+          <span key={ing}>{ing}</span>
         ))}
       </div>
 
@@ -73,6 +170,7 @@ export default function RecipeDetails({ recipe, AMIR }) {
         <h3>Method:</h3>
         <div>{documentToReactComponents(method)}</div>
       </div>
+
       <style jsx>{`
         h2,
         h3 {
